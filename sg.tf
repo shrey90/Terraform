02dea644793013bd244f7081b4a139c90dd185a5
Shrey Shekhar <37169278+shrey90@users.noreply.github.com>
2025-07-06 20:59:18 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,9 @@
+resource "aws_security_group" "my_tf_sg" {
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
